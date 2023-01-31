94a0f1957bc0bd3aabcc126f47702ae8e871d33a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-31 21:59:43 +0200

    app is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,121 +1,206 @@
 import { fetchImages } from './fetch';
-import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGalleryList } from './Gallery/ImageGalleryList';
 import { ImageGalleryItem } from './Gallery/ImageGalleryItem';
 import { LoadMoreButton } from './LoadMoreButton/LoadMoreButton';
 import { Modal } from './Modal/Modal';
-import Notiflix from 'notiflix';
 import { Audio } from 'react-loader-spinner';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    images: [],
-    currentSearch: '',
-    isLoading: false,
-    showModal: false,
-    modalImage: null,
-    loadMoreButon: false,
-    page: 1,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [currentSearch, setCurrentSearch] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  const [loadMoreButton, setLoadMoreButton] = useState(false);
+  const [page, setPage] = useState(1);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.currentSearch !== this.state.currentSearch
-    ) {
-      fetchImages(this.state.currentSearch, this.state.page)
+  useEffect(() => {
+    if (currentSearch !== null) {
+      fetchImages(currentSearch, page)
         .then(foundData => {
-          if (foundData.hits == 0) {
-            Notiflix.Notify.failure('There is no images');
-          } else {
-            Notiflix.Notify.success(
-              `Hooray, we found ${
-                foundData.total - this.state.images.length
-              } images!`
-            );
-            this.setState(prevState => {
-              return {
-                images: [...prevState.images, ...foundData.hits],
-                loadMoreButon:
-                  this.state.page < Math.ceil(foundData.total / 12),
-              };
-            });
+          if (foundData.hits === 0) {
+            console.log(foundData);
           }
+          setIsLoading(true);
+          setImages(prevstate => [...prevstate, ...foundData.hits]);
+          setLoadMoreButton(page < Math.ceil(foundData.total / 12));
         })
         .catch(error => {
           console.log(error);
         })
         .finally(() => {
-          this.setState({
-            isLoading: false,
-          });
+          setIsLoading(false);
         });
     }
-  }
-  onFormSubmitFetch = data => {
-    this.setState({
-      images: [],
-      currentSearch: data,
-      showModal: false,
-      modalImage: null,
-      page: 1,
-    });
+  }, [page, currentSearch]);
+
+  useEffect(() => {
+    setImages([]);
+    setCurrentSearch(null);
+    setShowModal(false);
+    setModalImage(null);
+    setPage(1);
+  }, []);
+
+  const onFormSubmitFetch = data => {
+    setImages([]);
+    setCurrentSearch(data);
+    setShowModal(false);
+    setModalImage(null);
+    setPage(1);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  toggleModal = modalImage => {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal,
-      modalImage: null,
-    }));
+  const toggleModal = modalImage => {
+    setShowModal(prevState => !prevState.showModal);
+    setModalImage(null);
+
     if (modalImage) {
-      this.setState({ modalImage });
-      console.log(modalImage);
+      setModalImage(modalImage);
     }
   };
 
-  render() {
-    return (
-      <div className="ImageGalleryFind">
-        <Searchbar onSubmit={this.onFormSubmitFetch} />
-        <ImageGalleryList>
-          <ImageGalleryItem
-            images={this.state.images}
-            onClick={this.toggleModal}
-          />
-        </ImageGalleryList>
-        {this.state.isLoading && (
-          <Audio
-            height="80"
-            width="80"
-            radius="9"
-            color="orange"
-            ariaLabel="loading"
-            wrapperStyle
-            wrapperClassName
-          />
-        )}
-        {this.state.showModal && (
-          <Modal
-            onClose={this.toggleModal}
-            children={
-              <img
-                src={this.state.modalImage.webformatURL}
-                alt={this.state.modalImage.tags}
-              />
-            }
-          ></Modal>
-        )}
-        {this.state.loadMoreButon && (
-          <LoadMoreButton loadMore={this.loadMore} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ImageGalleryFind">
+      <Searchbar onSubmit={onFormSubmitFetch} />
+      <ImageGalleryList>
+        <ImageGalleryItem images={images} onClick={toggleModal} />
+      </ImageGalleryList>
+      {isLoading && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="orange"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClassName
+        />
+      )}
+      {showModal && (
+        <Modal
+          onClose={toggleModal}
+          children={<img src={modalImage.webformatURL} alt={modalImage.tags} />}
+        ></Modal>
+      )}
+      {loadMoreButton && <LoadMoreButton loadMore={loadMore} />}
+    </div>
+  );
+};
+// export class App extends Component {
+//   state = {
+//     images: [],
+//     currentSearch: '',
+//     isLoading: false,
+//     showModal: false,
+//     modalImage: null,
+//     loadMoreButon: false,
+//     page: 1,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.page !== this.state.page ||
+//       prevState.currentSearch !== this.state.currentSearch
+//     ) {
+//       fetchImages(this.state.currentSearch, this.state.page)
+//         .then(foundData => {
+//           if (foundData.hits == 0) {
+//             Notiflix.Notify.failure('There is no images');
+//           } else {
+//             Notiflix.Notify.success(
+//               `Hooray, we found ${
+//                 foundData.total - this.state.images.length
+//               } images!`
+//             );
+//             this.setState(prevState => {
+//               return {
+//                 images: [...prevState.images, ...foundData.hits],
+//                 loadMoreButon:
+//                   this.state.page < Math.ceil(foundData.total / 12),
+//               };
+//             });
+//           }
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         })
+//         .finally(() => {
+//           this.setState({
+//             isLoading: false,
+//           });
+//         });
+//     }
+//   }
+//   onFormSubmitFetch = data => {
+//     this.setState({
+//       images: [],
+//       currentSearch: data,
+//       showModal: false,
+//       modalImage: null,
+//       page: 1,
+//     });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   toggleModal = modalImage => {
+//     this.setState(prevState => ({
+//       showModal: !prevState.showModal,
+//       modalImage: null,
+//     }));
+//     if (modalImage) {
+//       this.setState({ modalImage });
+//       console.log(modalImage);
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className="ImageGalleryFind">
+//         <Searchbar onSubmit={this.onFormSubmitFetch} />
+//         <ImageGalleryList>
+//           <ImageGalleryItem
+//             images={this.state.images}
+//             onClick={this.toggleModal}
+//           />
+//         </ImageGalleryList>
+//         {this.state.isLoading && (
+//           <Audio
+//             height="80"
+//             width="80"
+//             radius="9"
+//             color="orange"
+//             ariaLabel="loading"
+//             wrapperStyle
+//             wrapperClassName
+//           />
+//         )}
+//         {this.state.showModal && (
+//           <Modal
+//             onClose={this.toggleModal}
+//             children={
+//               <img
+//                 src={this.state.modalImage.webformatURL}
+//                 alt={this.state.modalImage.tags}
+//               />
+//             }
+//           ></Modal>
+//         )}
+//         {this.state.loadMoreButon && (
+//           <LoadMoreButton loadMore={this.loadMore} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 export async function fetchImages(value, pageNumber) {
-  const responce = await axios.get(
+  const response = await axios.get(
     `https://pixabay.com/api/?q=${value}&page=1&key=31911886-b70f9395e6a878339225ca253&image_type=photo&orientation=horizontal&per_page=12&page=${pageNumber}`,
     { params: {} }
   );
+  if (response.data.total === 0) {
+    Notiflix.Notify.failure('There is no images');
+    return;
+  }
   // console.log('Responce from fetch.js', responce.data.hits);
-  return responce.data;
+  Notiflix.Notify.success(`Hooray, we found ${response.data.total} images`);
+  return response.data;
 }
